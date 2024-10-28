387abfe5986dff9b03665ae90cca305f357ad1e0
Miguel <miguel.tinembart@gmail.com>
2024-10-28 20:23:57 +0100

    feat: Model for github webhook created

--- src/api/webhook.py
+++ src/api/webhook.py
@@ -1,14 +1,11 @@
-from enum import Enum
-from typing import Any, Dict, Optional
+from typing import Any, Dict
 from fastapi import FastAPI
-import logging
 
-from pydantic import BaseModel
 app = FastAPI()
 
 @app.post("/events/{provider}")
 def react_to_webhook(provider: str, payload: Dict[Any, Any]):
-    
-    logging.info(payload)
     return {'event': "done"}
+
+
 
